b32fba57d38f1d852351af3a120a15b67c8e0a04
Aditya Swaroop <adi4uenjoy@gmail.com>
2011-12-09 11:10:53 +0530

    Salary Allotment Feature is added

--- app/models/salary_allotment.rb
+++ app/models/salary_allotment.rb
@@ -1,3 +1,14 @@
 class SalaryAllotment < ActiveRecord::Base
 
+  def salaryHead
+    SalaryHead.find(salary_head_id).head_name
+  end
+
+  def shortName
+    SalaryHead.find(salary_head_id).short_name
+  end
+
+  def salaryHeadType
+    SalaryHead.find(salary_head_id).salary_type
+  end
 end
