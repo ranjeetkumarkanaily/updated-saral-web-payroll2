0a02237933f573eaba05309b40b5a2ffb3169e41
Ranjeet <ranjeetkumarkanaily@gmail.com>
2011-12-12 11:21:44 +0530

    Salary view spec updated

--- spec/views/salaries/index.html.haml_spec.rb
+++ spec/views/salaries/index.html.haml_spec.rb
@@ -5,6 +5,7 @@ describe "salaries/index.html.haml" do
 
   it "render form to generate salary" do
     render
+    puts rendered
     assert_select "form", :action => "/salaries", :method => "post" do
       assert_select "select#month_year", :name => "month_year"
     end
