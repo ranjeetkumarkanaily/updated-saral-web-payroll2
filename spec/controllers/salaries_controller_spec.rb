90a3ebde0f14e6ccf0cf1468530ef5e12da59470
Ranjeet <ranjeetkumarkanaily@gmail.com>
2011-12-12 10:40:19 +0530

    Factory for salary

---
+++ spec/controllers/salaries_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+describe SalaryAllotmentsController do
+  before :each do
+    controller.stub(:logged_in?).and_return(true)
+  end
+
+  describe "POST create" do
+    it "creates a new salary" do
+      salary_allot = FactoryGirl.create(:salary_allotment)
+      salary = FactoryGirl.build(:salary)
+      expect {
+        post :create, :salary => [salary.attributes], :month_year => "02/2011", :employee_id => "1"
+      }.to change(Salary, :count).by(1)
+    end
+  end
+end
--- spec/views/salaries/index.html.haml_spec.rb
+++ spec/views/salaries/index.html.haml_spec.rb
@@ -10,29 +10,4 @@ describe "salaries/index.html.haml" do
     end
   end
 
-  describe "Generated Salary" do
-    before :each do
-      @salary = Factory(:salary)
-      view.stub!(:params).and_return :month_year => @salary.month_year
-    end
-
-    it 'should show generated salary of employee for assigned month_year' do
-      assign(:genSal, [stub_model(Salary,
-                                  :employee_id => @salary.employee.id,
-                                  :basic => @salary.basic,
-                                  :da => @salary.da,
-                                  :hra => @salary.hra,
-                                  :conveyance => @salary.conveyance,
-                                  :allowance => @salary.allowance)])
-
-      render
-      rendered.should have_content(@salary.employee.id)
-    end
-
-    it "should not show salary for unassigned month_year" do
-      assign(:genSal,[])
-      render
-      rendered.should have_content("No Record Found")
-    end
-  end
 end
