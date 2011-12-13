6580a451d601c166d61a7f1e2232caeba8073294
ganesh <ganesh@Ganesh.(none)>
2011-12-13 17:39:27 +0530

    Added validations to model of employee details and changed the caption for new Employee details

--- spec/models/employee_detail_spec.rb
+++ spec/models/employee_detail_spec.rb
@@ -6,4 +6,10 @@ describe EmployeeDetail do
   it { should belong_to(:salary_group)}
 
   it { should have_many(:salary_allotments)}
+
+  it "checks for the uniqueness of employee_id and effective_date" do
+    emp_detail = FactoryGirl.create(:employee_detail)
+    duplicate_emp_detail = EmployeeDetail.new(:employee_id => 1, :effective_date => "2009-10-31", :salary_group_id =>1, :allotted_gross=>5000)
+    duplicate_emp_detail.should_not be_valid
+  end
 end
