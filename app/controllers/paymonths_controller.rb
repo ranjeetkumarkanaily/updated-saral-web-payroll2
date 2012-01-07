37e4df1c8d42caf6192dbe33846da4c8a493d6bf
Rohini <rohini@rohini.(none)>
2012-01-07 14:12:53 +0530

    refactoring paymonth

--- app/controllers/paymonths_controller.rb
+++ app/controllers/paymonths_controller.rb
@@ -18,28 +18,28 @@ class PaymonthsController < ApplicationController
 
   def create
 
-    tmp_month_year = params[:paymonth]
-    tmp_month_year = tmp_month_year[:month_name]
-    res =  Paymonth.proceed_to_save tmp_month_year
-
-    if res[0]
-      params_to_save = Paymonth.find_month_details_to_save tmp_month_year
-      @paymonth = Paymonth.new(:month_year => params_to_save[0],:number_of_days => params_to_save[1],:from_date => params_to_save[2],:to_date => params_to_save[3],:month_name => params_to_save[4])
-      respond_to do |format|
-        if @paymonth.save
-          format.html { redirect_to @paymonth, notice: 'Paymonth was successfully created.' }
-          format.json { render json: @paymonth, status: :created, location: @paymonth }
-        else
-          format.html { render action: "new" }
-          format.json { render json: @paymonth.errors, status: :unprocessable_entity }
+    tmp_month_year = params[:paymonth][:month_name]
+    if tmp_month_year.valid?
+      res =  Paymonth.proceed_to_save tmp_month_year
+      if res[0]
+        params_to_save = Paymonth.find_month_details_to_save tmp_month_year
+        @paymonth = Paymonth.new(:month_year => params_to_save[0],:number_of_days => params_to_save[1],:from_date => params_to_save[2],:to_date => params_to_save[3],:month_name => params_to_save[4])
+        respond_to do |format|
+          if @paymonth.save
+            format.html { redirect_to @paymonth, notice: 'Paymonth was successfully created.' }
+            format.json { render json: @paymonth, status: :created, location: @paymonth }
+          else
+            format.html { render action: "new" }
+            format.json { render json: @paymonth.errors, status: :unprocessable_entity }
+          end
         end
-      end
-    else
-      @paymonth = Paymonth.new
-      @paymonth.errors.add(:paymonth, "Out of sequence Month/Year can not be created. Next Month to be created is #{res[1]}")
+      else
+        @paymonth = Paymonth.new
+        @paymonth.errors.add(:paymonth, "Out of sequence Month/Year can not be created. Next Month to be created is #{res[1]}")
 
-      respond_to do |format|
-        format.html { render action: "new"}
+        respond_to do |format|
+          format.html { render action: "new"}
+        end
       end
     end
   end
