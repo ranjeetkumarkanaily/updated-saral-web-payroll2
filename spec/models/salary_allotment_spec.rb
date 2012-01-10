92a0588ef7af03e04e362a60951dd8d8daef7e89
Aditya Swaroop <adi4uenjoy@gmail.com>
2012-01-10 11:07:59 +0530

    Test code written for new salary module

--- spec/models/salary_allotment_spec.rb
+++ spec/models/salary_allotment_spec.rb
@@ -7,7 +7,7 @@ describe SalaryAllotment do
 
   it "should return allotted salaries" do
     sal_allot = FactoryGirl.create(:salary_allotment)
-    SalaryAllotment.get_allotted_salaries("02/2011",sal_allot.employee_id).should be_true
+    SalaryAllotment.get_allotted_salaries("Feb/2011",sal_allot.employee_id).should be_true
   end
 
   it "should return allotted salaries for previous effective date" do
