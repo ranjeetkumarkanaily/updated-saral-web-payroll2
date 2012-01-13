95c5f6d99e99042dc62f9dc2ebb3c8aeb269d0bd
ranjeet kumar <ranjeetkumarkanaily@gmail.com>
2012-01-13 10:11:13 +0530

    Pt Rate deletion is complete

--- app/models/pt_rate.rb
+++ app/models/pt_rate.rb
@@ -3,19 +3,38 @@ class PtRate < ActiveRecord::Base
   belongs_to :paymonth
 
   validates_presence_of :PtGroup_id, :paymonth_id
-  before_validation :check_max_min , :check_ranges
-
+  before_validation :populate_max_sal_range
+  before_destroy :update_rows
   private
 
-    def check_max_min
-      self.errors.add(:min_sal_range, "Minimum should be lesser than Maximum!") unless self.max_sal_range > self.min_sal_range
+    def update_rows
+      if self.id == PtRate.first.id
+        PtRate.first.delete
+      elsif self.id == PtRate.last.id
+        previous_row.update_attribute(:max_sal_range , 0)
+      else
+        previous_row.update_attribute(:max_sal_range, (next_row.min_sal_range - 0.01))
+      end
     end
 
-    def check_ranges
-      row = PtRate.find(:last, :conditions => ["id != #{self.id.nil? ? 0 : self.id}  and (paymonth_id = #{paymonth_id})"])
+    def populate_max_sal_range
+      self.max_sal_range = 0
+      row = PtRate.find(:last, :conditions => {:paymonth_id => self.paymonth_id})
       if row
-        self.errors.add(:salary, "Out of range") unless self.min_sal_range > row.max_sal_range
+        if self.min_sal_range > row.min_sal_range.to_f
+          row.update_attribute(:max_sal_range , (self.min_sal_range - 0.01))
+        else
+          self.errors.add(:salary, " range is already exists.")
+        end
       end
+    end
+
+    def previous_row
+      PtRate.where("id < ?", self.id).order("id DESC").first
+    end
+
+    def next_row
+      PtRate.where("id > ?", self.id).first
     end
 
 end
