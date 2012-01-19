61ee5f67f12f4d985d03b27dcae93f6e93f75931
ranjeet kumar <ranjeetkumarkanaily@gmail.com>
2012-01-19 18:05:29 +0530

    pushing into developer branch

--- app/models/pt_rate.rb
+++ app/models/pt_rate.rb
@@ -3,71 +3,60 @@ class PtRate < ActiveRecord::Base
   belongs_to :paymonth
 
   validates_presence_of :PtGroup_id, :paymonth_id
-  before_validation :populate_max_sal_range
-  after_validation :check_previous_range
-  before_destroy :update_rows
+  validates :min_sal_range, :uniqueness => true
 
-  after_create :update_previous_row
-  after_update :update_previous_row
+  before_validation :create_ranges_on_sal_ranges
+
+  #after_create :update_previous_row_max_sal_range
+ # after_update :update_previous_row_max_sal_range#, :update_self_max_sal_range
+
+  #before_update :update_self_max_sal_range
+
+  #before_destroy :update_rows
 
 
-  private
-
-    def update_rows
-      #if self.id == PtRate.first.id
-      #  PtRate.first.delete
-      #elsif self.id == PtRate.last.id
-      #  previous_row.update_attribute(:max_sal_range , 0)
-      #else
-      #  previous_row.update_attribute(:max_sal_range, (next_row.min_sal_range - 0.01))
-      #end
+    def get_max_sal_range
+      next_row = next_row_on_min_sal_range
+      (next_row.min_sal_range.to_f - 0.01) if next_row
     end
 
-    def check_previous_range
-      if self.id
-        row = previous_row
-      else
-        row = PtRate.find(:last, :conditions => {:paymonth_id => self.paymonth_id})
-      end
-
-      if row
-        if self.min_sal_range < row.min_sal_range.to_f
-          self.errors.add(:salary, " range is already exists.")
-        else
-          if next_row
-            self.max_sal_range = next_row.min_sal_range - 0.01
-          end
-
-        end
-      end
+    def create_ranges_on_sal_ranges
+      PtRate.select("pt").where("  ")
     end
 
-    def update_previous_row
-      if previous_row
-        if self.min_sal_range > previous_row.min_sal_range.to_f
-          previous_row.update_attribute(:max_sal_range , (self.min_sal_range - 0.01))
-        else
-          self.errors.add(:salary, " range is already exists.")
-        end
-      end
+
+   private
+
+    def next_row_on_min_sal_range
+      PtRate.where("min_sal_range > ? and paymonth_id = ?", self.min_sal_range, self.paymonth_id).order("min_sal_range").first
     end
 
-    def update_self_max_range
-      if next_row
-        self.max_sal_range = next_row.min_sal_range - 0.01
-      end
-    end
-
-    def populate_max_sal_range
-      self.max_sal_range = 0
-    end
-
-    def previous_row
-      PtRate.where("created_at < ? and paymonth_id = ?", self.created_at, self.paymonth_id).order("created_at DESC").first
-    end
-
-    def next_row
-      PtRate.where("created_at > ? and paymonth_id = ?", self.created_at, self.paymonth_id).order("created_at").first
-    end
+    #def prev_row_on_min_sal_range
+    #  PtRate.where("min_sal_range < ? and paymonth_id = ?", self.min_sal_range, self.paymonth_id).order("min_sal_range DESC").first
+    #end
+    #
+    #def update_previous_row_max_sal_range
+    #  prev_row = prev_row_on_min_sal_range
+    #  prev_row.update_attribute(:max_sal_range, (self.min_sal_range - 0.01)) if prev_row
+    #end
+    #
+    #def update_self_max_sal_range
+    #  next_row = next_row_on_min_sal_range
+    #  self.max_sal_range = (next_row.min_sal_range - 0.01) if next_row
+    #end
+    #
+    #def update_rows
+    #  if self.id == PtRate.order("min_sal_range").first.id
+    #    PtRate.order("min_sal_range").first.delete
+    #  elsif self.id == PtRate.order("min_sal_range DESC").first.id
+    #    puts "kanaily"
+    #    puts self.id
+    #    prev_row = prev_row_on_min_sal_range
+    #    puts prev_row.inspect
+    #    prev_row.update_attribute(:max_sal_range , 0)
+    #  else
+    #    prev_row_on_min_sal_range.update_attribute(:max_sal_range, (next_row_on_min_sal_range.min_sal_range - 0.01))
+    #  end
+    #end
 
 end
