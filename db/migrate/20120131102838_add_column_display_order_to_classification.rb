1e32c865a5e4643717dc6fb353e3cd8102e8b158
Aditya Swaroop <adi4uenjoy@gmail.com>
2012-02-03 12:16:58 +0530

    Removing Unwanted migration file

--- db/migrate/20120131102838_add_column_display_order_to_classification.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnDisplayOrderToClassification < ActiveRecord::Migration
-  def change
-    add_column :classifications, :display_order, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20120201123338) do
+ActiveRecord::Schema.define(:version => 20120202050611) do
 
   create_table "attendance_configurations", :force => true do |t|
     t.string   "attendance"
