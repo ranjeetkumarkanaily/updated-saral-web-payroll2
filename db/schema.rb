8f6070d8e94b7a07cf5942fd55bf12b6fb550ff4
RSL <rsl@Serveru.(none)>
2012-02-09 10:31:34 +0530

    Merging Conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20120203104107) do
 
   create_table "attendance_configurations", :force => true do |t|
@@ -156,16 +155,6 @@ ActiveRecord::Schema.define(:version => 20120203104107) do
     t.integer  "employee_id"
     t.string   "month_year"
     t.float    "lop"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "month_years", :force => true do |t|
-    t.integer  "month_year"
-    t.integer  "number_of_days"
-    t.date     "from_date"
-    t.date     "to_date"
-    t.string   "month_name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
