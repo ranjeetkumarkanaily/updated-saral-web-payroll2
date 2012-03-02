57596fcd7f4cac8596fedc70f06b4d0cbae6518a
ranjeet <ranjeetkumarkanaily@gmail.com>
2012-03-02 17:00:35 +0530

    Taking updates

--- db/schema.rb
+++ db/schema.rb
@@ -199,16 +199,6 @@ ActiveRecord::Schema.define(:version => 20120215042312) do
     t.datetime "updated_at"
   end
 
-  create_table "month_years", :force => true do |t|
-    t.integer  "month_year"
-    t.integer  "number_of_days"
-    t.date     "from_date"
-    t.date     "to_date"
-    t.string   "month_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "paymonths", :force => true do |t|
     t.integer  "month_year"
     t.integer  "number_of_days"
