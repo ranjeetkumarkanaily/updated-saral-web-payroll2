05db61f56cde03728e3c5e17917e8881f47bdc18
Aditya Swaroop <adi4uenjoy@gmail.com>
2012-03-09 17:27:24 +0530

    Employee Ajax Search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,15 +9,6 @@
 //= require_tree .
 
 $(function() {
-//  $("#emp_search").submit(function() {
-//    $.get(this.action, $(this).serialize(), null, "script");
-//    return false;
-//  });
-
-//  $("#products th a, #products .pagination a").live("click", function() {
-//    $.getScript(this.href);
-//    return false;
-//  });
 
   $("#search_field").keyup(function() {
     $.get($("#emp_search").attr("action"), $("#emp_search").serialize(), null, "script");
