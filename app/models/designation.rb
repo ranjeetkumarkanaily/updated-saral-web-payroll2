7f2114fb2cefba7d01b852308b2e7b4a4d29d1a6
Aditya Swaroop <adi4uenjoy@gmail.com>
2012-03-14 10:38:02 +0530

    Deleted Department, Designation, Grade module

--- app/models/designation.rb
+++
@@ -1,6 +0,0 @@
-class Designation < ActiveRecord::Base
-  validates :designation,   :presence   => true,
-            :uniqueness => { :case_sensitive => false }
-
-  has_many :employees
-end
--- app/models/grade.rb
+++
@@ -1,6 +0,0 @@
-class Grade < ActiveRecord::Base
-  validates :grade,   :presence   => true,
-            :uniqueness => { :case_sensitive => false }
-
-  has_many :employees
-end
