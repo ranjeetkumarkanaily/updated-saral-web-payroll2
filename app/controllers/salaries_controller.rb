5a55832c702c59d5b22b5da33307a668efa1e00c
Aditya Swaroop <adi4uenjoy@gmail.com>
2012-03-19 15:06:51 +0530

    Removing PF/ESI head from display

--- app/controllers/salaries_controller.rb
+++ app/controllers/salaries_controller.rb
@@ -117,7 +117,6 @@ class SalariesController < ApplicationController
         end
       end
     end
-
   end
 
 end
--- app/controllers/salary_heads_controller.rb
+++ app/controllers/salary_heads_controller.rb
@@ -2,7 +2,7 @@ class SalaryHeadsController < ApplicationController
   # GET /salary_heads
   # GET /salary_heads.json
   def index
-    @salary_heads = SalaryHead.order('created_at ASC').paginate(:page => params[:page], :per_page => 10)
+    @salary_heads = SalaryHead.where('id != 2 and id != 3').order('created_at ASC').paginate(:page => params[:page], :per_page => 10)
 
     respond_to do |format|
       format.html # salary_sheet.html.haml
