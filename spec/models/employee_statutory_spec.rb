cb2c8caf2f9d6480f3b310225a604ab7339a5c5b
Rohini <rohini@rohini.(none)>
2012-04-25 13:01:30 +0530

    added statutory details for employee

--- spec/models/employee_statutory_spec.rb
+++ spec/models/employee_statutory_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe EmployeeStatutory do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @employee = FactoryGirl.create(:employee)
+    @attr = {
+        :employee => @employee,
+        :pan => "aaaaa1234a",
+        :pan_effective_date => "2012-01-01",
+        :pf => "123pf",
+        :pf_effective_date => "2012-01-01",
+        :esi => "A1234",
+        :esi_effective_dat => "2012-01-01",
+    }
+  end
+
+  it { should belong_to(:employee)}
+
 end
