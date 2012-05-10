ac6f62f5f3999f2c825d745435ed156635daf889
Vinay KP <Vinay KP>
2012-05-10 12:37:20 +0530

    changes in companu document model

--- app/models/company_document.rb
+++ app/models/company_document.rb
@@ -2,13 +2,13 @@ class CompanyDocument < ActiveRecord::Base
   attr_accessible :company_id,:file_path,:remarks
   belongs_to :company
   has_attached_file :file_path
-  @file_specs=UploadFileType.first
-  validates_attachment_size :file_path, :less_than => @file_specs.max_file_size.megabyte
-  if @file_specs.file_type =="all"
-    validates_attachment_content_type :file_path
-  else
-    validates_attachment_content_type :file_path, :content_type => UploadFileType.validate_file_type(@file_specs.file_type)
-  end
+  #@file_specs=UploadFileType.first
+  #validates_attachment_size :file_path, :less_than => @file_specs.max_file_size.megabyte
+  #if @file_specs.file_type =="all"
+  #  validates_attachment_content_type :file_path
+  #else
+  #  validates_attachment_content_type :file_path, :content_type => UploadFileType.validate_file_type(@file_specs.file_type)
+  #end
 
   validates :file_path_file_name, presence: true
 end
