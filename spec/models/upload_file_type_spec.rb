68f5256e628322a20ed20d159274e7b6b70c6068
Vinay KP <Vinay KP>
2012-05-11 12:57:23 +0530

    upload file type spec

--- spec/models/upload_file_type_spec.rb
+++ spec/models/upload_file_type_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe UploadFileType do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should return array values" do
+    file_type=FactoryGirl.create(:upload_file_type)
+    values = UploadFileType.validate_file_type(file_type.file_type)
+    values.should eq(["application/pdf", "application/msword"])
+  end
 end
