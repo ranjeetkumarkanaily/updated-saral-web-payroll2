10e8243eed6e871850c4bfa994a1fbb18a9f0d82
ranjeet <ranjeetkumarkanaily@gmail.com>
2012-05-24 11:30:52 +0530

    Option setting, Hr Master refactored

--- spec/models/hr_master_spec.rb
+++ spec/models/hr_master_spec.rb
@@ -7,9 +7,9 @@ describe HrMaster do
 
   it "should set error hash" do
     first_catgry_det = FactoryGirl.create(:hr_category_detail)
-    FactoryGirl.create(:hr_category_detail, :hr_category_id => first_catgry_det.hr_category_id, :name => "Age", :data_type => "String", :required => false)
+    FactoryGirl.create(:hr_category_detail, :hr_category_id => first_catgry_det.hr_category_id, :name => "Age", :data_type => "String", :required => true)
     hr_master = HrMaster.new(:hr_category_id => first_catgry_det.hr_category_id, :category_values => "Name => Kanaily")
     hr_master.save
-    hr_master.errors.full_messages.should eq(["Hr master category values for \"Name\" is required"])
+    hr_master.errors.full_messages.should eq(["Hr master category values for \"Age\" is required"])
   end
 end
