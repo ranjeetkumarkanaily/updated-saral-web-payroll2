f1529b8f6dce4cc882d8f9fb1481cc276a1c5f1b
Aditya Swaroop <adi4uenjoy@gmail.com>
2012-06-07 15:43:54 +0530

    Salary Alltoment edit page testing

--- spec/views/salary_allotments/edit.html.haml_spec.rb
+++ spec/views/salary_allotments/edit.html.haml_spec.rb
@@ -3,13 +3,19 @@ require 'spec_helper'
 describe "salary_allotments/edit" do
 
   it "render form contains salary heads for selected employee" do
-    employee = FactoryGirl.create(:employee)
     attendance_configuration = FactoryGirl.create(:attendance_configuration)
     branch = FactoryGirl.create(:branch)
     financial_institution = FactoryGirl.create(:financial_institution)
-    employee_details = FactoryGirl.create(:employee_detail, :employee_id => employee.id, :attendance_configuration_id=>attendance_configuration.id,:branch_id=>branch.id, :financial_institution_id=>financial_institution.id)
     salary_head = FactoryGirl.create(:salary_head)
-    salary_group_detail = FactoryGirl.create(:salary_group_detail,:salary_head_id=>salary_head.id,:calc_type=>"Every Month")
+    salary_group = FactoryGirl.create(:salary_group)
+    salary_group_detail = FactoryGirl.create(:salary_group_detail,:salary_head_id=>salary_head.id,:salary_group_id=>salary_group.id,:calc_type=>"Every Month")
+    employee = FactoryGirl.create(:employee)
+    employee_details = FactoryGirl.create(:employee_detail,
+                                          :employee_id => employee.id,
+                                          :salary_group_id=>salary_group.id,
+                                          :attendance_configuration_id=>attendance_configuration.id,
+                                          :branch_id=>branch.id,
+                                          :financial_institution_id=>financial_institution.id)
     salAllot = FactoryGirl.create(:salary_allotment, :employee_id => employee.id, :salary_group_detail_id => salary_group_detail.id,:salary_head_id=>salary_head.id)
     view.stub!(:params).and_return :id => salAllot.employee_id
 
